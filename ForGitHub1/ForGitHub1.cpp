7015a5889f191daf9ee5cc6070dfeb23b29a55c1
Jambi <iturkas42@gmail.com>
2024-03-08 03:28:28 +0200

    Added "usinng namespace std" and thus the code looks simpler.

--- ForGitHub1/ForGitHub1.cpp
+++ ForGitHub1/ForGitHub1.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
 
 	int a;
 	int b;
 
-	std::cout << "Write your first number down below: " << std::endl;
-	std::cin >> a;
-	std::cout << "Write your second numder down below: " << '\n';
-	std::cin >> b;
+	cout << "Write your first number down below: " << endl;
+	cin >> a;
+	cout << "Write your second numder down below: " << '\n';
+	cin >> b;
 
 	int c;
 
@@ -16,18 +18,18 @@ int main() {
 
 	if (c < 10) {
 
-		std::cout << "The sum of ur two numbers is less than 10" << '\n';
-		std::cout << "The sum is " << c << '\n';
+		cout << "The sum of ur two numbers is less than 10" << '\n';
+		cout << "The sum is " << c << '\n';
 	}
 	else if (c == 10) {
 
-		std::cout << "The sum of ur two numbers is 10" << '\n';
-		std::cout << "The sum is " << c << '\n';
+		cout << "The sum of ur two numbers is 10" << '\n';
+		cout << "The sum is " << c << '\n';
 	}
 	else {
 
-		std::cout << "The sum of ur two numbers is greater than 10" << '\n';
-		std::cout << "The sum is " << c << '\n';
+		cout << "The sum of ur two numbers is greater than 10" << '\n';
+		cout << "The sum is " << c << '\n';
 	}
 
 	return 0;
